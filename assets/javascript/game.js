15552fca52adb5c2d3f3ce18a3199de76a2e2f7c
venu.mora <venukumar.mora@gmail.com>
2017-11-11 12:10:04 -0800

    Converting to prototype pattern and fix some UI issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
-(function($) { 
+(function($) {
+	'use strict';
+	
 	let states = [
 	{ id:'US-AK', name: 'Alaska' },
 	{ id:'US-AL', name: 'Alabama' },
@@ -51,28 +53,9 @@
 	{ id:'US-WI', name: 'Wisconsin' },
 	{ id:'US-WV', name: 'West Virginia' },
 	{ id:'US-WY', name: 'Wyoming' }
-	],
-	currentState = {},
-	playerName = '',
-	animateFill = null,
-	gameStarted = false,
-	hangmanText = '',
-	lives = 6,
-	previousGuesses = [];
+	];
 
-	const lands = document.querySelectorAll('.land'),
-	hangmanTextElement = document.getElementById('hangman'),
-	takeaway = document.getElementById('takeaway'),
-	livesElement = document.getElementById('lives');
-
-	String.prototype.replaceAt = function(index, replacement) {
-    	return this.substr(0, index) + replacement + this.substr(index + replacement.length);
-	}
-
-	playerName = prompt('Hello!! What should I call you?');
-	playerName = playerName || 'Anonymous';
-	takeaway.innerHTML = `Hi <strong>${playerName}</strong> Press any key to Start!! <i class="fa fa-play" aria-hidden="true"></i>`;
-
+	//	Gets random or specific state object.
 	function getRandomState(id) {
 		if(id) {
 			return states[id];
@@ -80,106 +63,158 @@
 		return states[Math.floor(Math.random()*states.length)];
 	}
 
+	// Removes state object from states array.
 	function removeState(state) {
 		states.splice(states.indexOf(state), 1);
 	}
 
-	function fillLand() {
-		lands.forEach(function(path){
-			path.classList.remove('fill');
-		});
-		document.getElementById(getRandomState().id).classList.add('fill');
+	// Extend strings to replace character at index.
+	String.prototype.replaceAt = function(index, replacement) {
+		return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 	}
 
-	function startGame() {
-		gameStarted = true;
-		currentState = getRandomState();
-		removeState(currentState);
-		animateFill = setInterval(function() {
-				fillLand();
-		}, 1000);
-		hangmanTextElement.classList.remove('lost');
-		hangmanTextElement.classList.remove('won');
-		hangmanText = '';
-		lives = 6;
-		previousGuesses = [];
-		livesElement.innerHTML = lives;
-		takeaway.innerHTML = `Let\'s play Hangman!! <strong>${playerName}</strong>, Start typing letters. Guess the state of the USA`;
-		hangmanText = currentState.name.replace(/[\s]/g, '  ').replace(/[a-z]/gi, '_ ');
-		hangmanTextElement.innerHTML = hangmanText.replace(/\s\s/g, '&nbsp;&nbsp;');
-		document.getElementById('sofarContainer').innerHTML = '<p>Letters guessed so far: <span id="sofar"></span></p>';
-	}
+	
+	var hangmanGame = function() {
+		this.currentState = {},
+		this.playerName = '',
+		this.animateFill = null,
+		this.gameStarted = false,
+		this.hangmanText = '',
+		this.lives = 6,
+		this.previousGuesses = [];
 
-	function endGame(isWin) {
-		document.getElementById(currentState.id).classList.add(isWin ? 'won' : 'lost');
-		document.getElementById(currentState.id).classList.remove(isWin ? 'lost' : 'won');
-		lands.forEach(function(path){
-			path.classList.remove('fill');
-		});
-		hangmanTextElement.innerHTML = currentState.name;
-		if(isWin) {
-			takeaway.innerHTML = `${playerName} you are Awesome!<p>Enter any key to restart the Game</p>`;
-			hangmanTextElement.classList.add('won');
-			hangmanTextElement.classList.remove('lost');
-		} else {
-			takeaway.innerHTML = `Never mind try again!<p>Enter any key to restart the Game</p>`;
-			hangmanTextElement.classList.add('lost');
-			hangmanTextElement.classList.remove('won');
+		this.lands = document.querySelectorAll('.land');
+		this.hangmanTextElement = document.getElementById('hangman');
+		this.takeaway = document.getElementById('takeaway');
+		this.livesElement = document.getElementById('lives');
+
+		this.playerName = prompt('Hello!! What should I call you?');
+		this.playerName = this.playerName || 'Anonymous';
+		this.takeaway.innerHTML = `Hi <strong>${this.playerName}</strong> Press any key to Start!! <i class="fa fa-play" aria-hidden="true"></i>`;
+
+		// Fills color to random states on svg.
+		hangmanGame.prototype.fillLand = () => {
+			this.lands.forEach(function(path){
+				path.classList.remove('fill');
+			});
+			document.getElementById(getRandomState().id).classList.add('fill');
 		}
 
-		if(animateFill) {
-			clearInterval(animateFill);
-			animateFill = null;
+		hangmanGame.prototype.startGame = () => {
+			this.gameStarted = true;
+			this.currentState = getRandomState();
+			if(this.currentState) {
+				// Remove selected state so that a state is not repeated in single game.
+				removeState(this.currentState);
+				this.animateFill = setInterval(function() {
+					this.fillLand();
+				}.bind(this), 1000);
+
+				this.hangmanTextElement.classList.remove('lost');
+				this.hangmanTextElement.classList.remove('won');
+				this.hangmanText = '';
+				this.lives = 6;
+				this.previousGuesses = [];
+				this.livesElement.innerHTML = this.lives;
+				this.takeaway.innerHTML = `Let\'s play Hangman!! <strong>${this.playerName}</strong>, Start typing letters. Guess the state of the USA`;
+				this.hangmanText = this.currentState.name.replace(/[\s]/g, '  ').replace(/[a-z]/gi, '_ ');
+				this.hangmanTextElement.innerHTML = this.hangmanText.replace(/\s\s/g, '&nbsp;&nbsp;');
+				document.getElementById('sofarContainer').innerHTML = '<p>Letters guessed so far: <span id="sofar"></span></p>';
+				return;
+			}
+			this.takeaway.innerHTML = `<strong>${this.playerName}</strong> You completed guessing all the states.`;
+			this.hangmanText = 'Game Over';
+			this.hangmanTextElement.innerHTML = this.hangmanText;
 		}
 
-		gameStarted = false;
-	}
+		hangmanGame.prototype.registerGame = () => {
+			document.addEventListener('keyup', this.handleKeyUp.bind(this));
+		}
 
-	document.addEventListener('keyup', (event) => {
-		event.preventDefault();
-		if(!gameStarted) {
-			startGame();
+		hangmanGame.prototype.endGame = (isWin) => {
+			document.getElementById(this.currentState.id).classList.add(isWin ? 'won' : 'lost');
+			document.getElementById(this.currentState.id).classList.remove(isWin ? 'lost' : 'won');
+			this.gameStarted = false;
+
+			this.lands.forEach(function(path){
+				path.classList.remove('fill');
+			});
+			this.hangmanTextElement.innerHTML = this.currentState.name;
+			if(isWin) {
+				this.takeaway.innerHTML = `${this.playerName} you are Awesome!<p>Enter any key to restart the Game</p>`;
+				this.hangmanTextElement.classList.add('won');
+				this.hangmanTextElement.classList.remove('lost');
+			} else {
+				this.takeaway.innerHTML = `Never mind try again!<p>Enter any key to restart the Game</p>`;
+				this.hangmanTextElement.classList.add('lost');
+				this.hangmanTextElement.classList.remove('won');
+			}
+
+			if(this.animateFill) {
+				clearInterval(this.animateFill);
+				this.animateFill = null;
+			}
+		}
+
+		hangmanGame.prototype.handleKeyUp = (event) => {
+			event.preventDefault();
+			if(!this.gameStarted) {
+				this.startGame();
+				return;
+			}
+			// Determines which key was pressed.
+			let userGuess = event.key;
+
+			// When user guess is an alphabet
+			if(userGuess.match(/^[a-z]$/i)) {
+				// When user input is not already given.
+				if(this.previousGuesses.indexOf(userGuess) === -1) {
+					let isWin = false, regEx = null, counter = 0;
+					regEx = (new RegExp(userGuess,'gi'));
+
+					// Replace _ with actual letter in each occurance.
+					let match = '';
+					while ((match = regEx.exec(this.currentState.name))) {
+						counter++;
+						const actualIndex = 2 * match.index;
+						this.hangmanText = this.hangmanText.replaceAt(actualIndex, match[0]);
+					}
+
+					this.hangmanTextElement.innerHTML = this.hangmanText.replace(/\s\s/g, '&nbsp;&nbsp;');
+					this.takeaway.innerHTML = `<strong>${counter}</strong> match(es)${counter ? ' Sweet!!' : ''}`;
+
+					// When there is no match
+					// reduce lives
+					if(!counter) {
+						this.lives--;
+						this.livesElement.innerHTML = this.lives;
+					}
+
+					this.previousGuesses.push(userGuess);
+					document.getElementById('sofar').innerHTML = this.previousGuesses.join(', ');
+
+					isWin = !this.hangmanText.match(/[_]/g);
+
+					// When lives are 0 or user won the game
+					// call end procedure
+					if(!this.lives || isWin) {
+						this.endGame(isWin);
+					}	
+
+					return;
+				}
+
+				this.takeaway.innerHTML = `<strong>${userGuess}</strong> is already entered!!`;
+				return;
+			}
+
+			this.takeaway.innerHTML = `<strong>${userGuess === ' ' ? 'Space' : userGuess}</strong> is not valid, Google "How to play hangman?" !!`;
 			return;
 		}
-		// Determines which key was pressed.
-		let userGuess = event.key;
-		if(userGuess.match(/^[a-z]$/i)) {
-			if(previousGuesses.indexOf(userGuess) === -1) {
-				let isWin = false, regEx = null, counter = 0;
-				regEx = (new RegExp(userGuess,'gi'));
+	}
 
-				while ((match = regEx.exec(currentState.name))) {
-					counter++;
-					const actualIndex = 2 * match.index;
-					hangmanText = hangmanText.replaceAt(actualIndex, match[0]);
-				}
 
-				hangmanTextElement.innerHTML = hangmanText.replace(/\s\s/g, '&nbsp;&nbsp;');
-				takeaway.innerHTML = `<strong>${counter}</strong> match(es)${counter ? ' Sweet!!' : ''}`;
-
-				if(!counter) {
-					lives--;
-					livesElement.innerHTML = lives;
-				}
-
-				previousGuesses.push(userGuess);
-				document.getElementById('sofar').innerHTML = previousGuesses.join(', ');
-
-				isWin = !hangmanText.match(/[_]/g);
-
-				if(!lives || isWin) {
-					endGame(isWin);
-		  		}
-
-		  		return;
-	  		}
-
-	  		takeaway.innerHTML = `<strong>${userGuess}</strong> is already entered!!`;
-		  	return;
-	  	}
-
-	  	takeaway.innerHTML = `<strong>${userGuess === ' ' ? 'Space' : userGuess}</strong> is not valid, Google "How to play hangman?" !!`;
-		return;
-	});
+	var game = new hangmanGame();
+	game.registerGame();
 
 })(jQuery);
